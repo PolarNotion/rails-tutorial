97e93a0a699b7df0339f74f2d46858c74cd2d89a
jessephillips <mr.jesse.phillips@gmail.com>
2018-02-21 12:02:25 -0500

    Model methods

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,2 +1,27 @@
 class Donation < ApplicationRecord
+  validates :amount_in_cents, presence: true
+  
+  before_save :_ensure_stripe_charge_id
+  
+  def amount_in_dollars
+    amount_in_cents / 100
+  end
+  
+  def transaction_amount_in_dollars
+    amount_in_dollars * 0.03
+  end
+  
+  # Class Methods
+  
+  def self.gross_amount_in_dollars
+    self.sum(:amount_in_cents) / 100
+  end
+  
+  private
+  
+  def _ensure_stripe_charge_id
+    unless stripe_charge_id.present?
+      self.stripe_charge_id = SecureRandom.hex(6)
+    end
+  end
 end
